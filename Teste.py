1a99c0109a91d1f5d74351b875e8c8d018d76939
maru <dykayano@gmail.com>
2025-03-10 19:10:00 -0300

    Update Teste.py

--- Teste.py
+++ Teste.py
@@ -1,16 +1,26 @@
-user_input = int(input("Digite um número inteiro: "))
-print(user_input)
-if user_input>0:
-    cond1="É positivo."
-elif user_input<0:
-    cond1="É negativo."
-else: cond1="É nulo."
-print(cond1)
-if user_input % 2 == 0:
-    cond2="É par."
-else: cond2 = "É ímpar."
-print(cond2)
-if user_input % 3 == 0:
-    cond3 = "É divisível por 3."
-else: cond3 = "Não é divisível por 3."
-print(cond3)
+while True:
+    print("Digite um número inteiro: ")
+    user_input = input("")
+    try:
+        val = int(user_input)
+    except ValueError:
+        print("Não é um número inteiro!")
+        print()
+        continue
+    else:
+        user_input=int(user_input)
+        if user_input>0:
+            cond1="é positivo."
+        elif user_input<0:
+            cond1="é negativo."
+        else: cond1="é nulo."
+        print(user_input, cond1)
+        if user_input % 2 == 0:
+            cond2="é par."
+        else: cond2 = "é ímpar."
+        print(user_input, cond2)
+        if user_input % 3 == 0:
+            cond3 = "é divisível por 3."
+        else: cond3 = "não é divisível por 3."
+        print(user_input, cond3)
+        print()
